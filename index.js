d4fc4c04aee05c2a11731f0205c83a852e4660a2
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-03 11:42:46 +0600

    event capturing and event bubbling are tested

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function yes() {
     const answer = document.getElementById("answer");
-    answer.innerText = "Ooo yeah ..."
+    answer.innerText = "Ooo yeah ...";
 }
 function offCourse() {
     const answer = document.getElementById("answer");
-    answer.innerText = "Off Course ..."
+    answer.innerText = "Off Course ...";
 }
 
 let counting = 0;
@@ -12,11 +12,19 @@ function plusButton() {
     counting++;
     const plusButton = document.getElementById("increase-decrease");
     plusButton.innerText = counting;
-
 }
 function minusButton() {
     counting--;
     const minusButton = document.getElementById("increase-decrease");
     minusButton.innerText = counting;
-
 }
+
+document.getElementById("grand-parents").addEventListener("click", function () {
+    console.log("grand-parents");
+}, true);
+document.getElementById("parents").addEventListener("click", function () {
+    console.log("parents");
+}, false);
+document.getElementById("child").addEventListener("click", function () {
+    console.log("child");
+}, false);
