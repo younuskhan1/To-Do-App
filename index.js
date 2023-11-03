596b5ed57b7a4be0e782347ba35f78afa8c701fe
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-03 09:41:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function yes() {
+    const answer = document.getElementById("answer");
+    answer.innerText = "Ooo yeah ..."
+}
+function offCourse() {
+    const answer = document.getElementById("answer");
+    answer.innerText = "Off Course ..."
+}
