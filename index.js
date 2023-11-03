875d9388689edd14e366584077f1d0c5e6f0f9e9
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-03 10:47:12 +0600

    counting buttons are added

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function offCourse() {
     const answer = document.getElementById("answer");
     answer.innerText = "Off Course ..."
 }
+
+let counting = 0;
+function plusButton() {
+    counting++;
+    const plusButton = document.getElementById("increase-decrease");
+    plusButton.innerText = counting;
+
+}
+function minusButton() {
+    counting--;
+    const minusButton = document.getElementById("increase-decrease");
+    minusButton.innerText = counting;
+
+}
